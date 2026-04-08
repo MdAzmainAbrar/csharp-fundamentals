c2ac197f6284c9375b670d5944c814ddd0c84886
mdazmainabrar <abrarmain.aa@gmail.com>
2026-04-08 16:07:37 +0600

    shopping cart

---
+++ Project11/Cart.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project11
+{
+    public class Cart
+    {
+        //field 
+        private string? _cartId;
+        private Dictionary<string, double> _items; //dictionary of <string,double> such as <"lolipoop",2.5>
+
+        //constructor
+
+        public Cart()
+        {
+
+        }
+        public Cart(string cartId)
+        {
+            _cartId = cartId;
+            _items = new Dictionary<string, double>();
+        }
+
+        //methods
+        public void AddItem(string item, double price)
+        {
+            _items.Add(item,price); //adds an item to the cart
+        }
+
+        public void RemoveItem(string item)
+        {
+            _items.Remove(item); //remove items from the cart
+        }
+
+        public double GetTotal()
+        {
+            double total = 0;
+            foreach (var item in _items) //(KeyValuePair<string, double> item in _items)
+            {
+                total += item.Value; //returns the total cost of the items in the cart
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            //StringBuilder sb = new StringBuilder();
+            //sb.AppendLine($"Cart ID: {_cartId}");
+            //sb.AppendLine("Items:");
+            //foreach (var item in _items)
+            //{
+            //    sb.AppendLine($"{item.Key}: ${item.Value}");
+            //}
+            //sb.AppendLine($"Total: ${GetTotal()}");
+            //return sb.ToString();
+            string result = $"Cart ID: {_cartId}\nItems:\n";
+            foreach (var item in _items)
+            {
+               result += $"{item.Key}: ${item.Value.ToString("F2")}\n";
+            }
+            result += $"Total {GetTotal().ToString("F2")}\n";
+            return result;
+        }
+    }
+}
---
+++ Project11/Program.cs
@@ -0,0 +1,29 @@
+﻿// create a cart class that represents a shopping cart in an ecommerce application. The cart class should have the following
+// instance fields: _cartId (string) _items (dictionary of <string,double> such as <"lolipoop",2.5> 
+// The cart class should have the following methods to
+// AddItems- adds an item to the cart,
+// RemoveItem - remove items from the cart,
+// GetTotal - returns the total cost of the items in the cart,
+
+using Project11;
+
+//Dictionary<string, double> items = new Dictionary<string, double>();
+//items.Add("lolipoop", 2.5);
+//items.Add("candy", 1.0);
+//items.Add("soda", 1.5);
+
+Cart cart1 = new Cart("12345");
+cart1.AddItem("lolipoop", 2.5);
+cart1.AddItem("candy", 1.0);
+cart1.AddItem("soda", 1.5);
+
+Console.WriteLine(cart1);
+cart1.RemoveItem("candy");
+Console.WriteLine(cart1);
+
+Cart cart2 = new Cart("44444");
+cart2.AddItem("chips", 3.0);
+cart2.AddItem("soda", 1.5);
+cart2.AddItem("candy", 1.0);
+Console.WriteLine(cart2);
+
